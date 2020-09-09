bf9d33737530a1a954c64d1dd4db541f12dedc46
Whyssz <dragoniiix@mail.ru>
2020-09-09 19:37:12 +0300

    added product slider

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,8 @@ gulp.task('style', () => {
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/rateyo/lib/iife/rateyo.css'
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -31,7 +32,8 @@ gulp.task('script', () => {
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        'node_modules/mixitup/dist/mixitup.js'
+        'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/rateyo/lib/iife/rateyo.js'
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
